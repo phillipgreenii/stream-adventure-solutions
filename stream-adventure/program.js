f551647598444949dac608d555c06b103feb9486
Phillip Green II <phillip.green.ii@gmail.com>
2014-09-13 08:15:56 -0400

    complete lines

--- stream-adventure/program.js
+++ stream-adventure/program.js
@@ -1,7 +1,23 @@
-var through = require('through');
-var tr = through(function(buf){
-  this.queue(buf.toString().toUpperCase());
-});
+var through = require('through'),
+    split = require('split');
 
+function lineCounter() {
+  var current = 0;
+  return through(function(line) {
+    this.queue([line, ++current]);
+  });
+}
 
-process.stdin.pipe(tr).pipe(process.stdout);
+process.stdin
+    .pipe(split())
+    .pipe(lineCounter())
+    .pipe(through(function (parts) {
+        line = parts[0];
+        count = parts[1];
+        if (count % 2 === 0) {
+          this.queue(line.toUpperCase() +"\n");
+        } else {
+          this.queue(line.toLowerCase() +"\n");
+        }
+    }))
+    .pipe(process.stdout);
