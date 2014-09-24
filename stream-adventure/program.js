aef5139f3d23d479f297f88f13a65a6b1d3c9fee
Phillip Green II <phillip.green.ii@gmail.com>
2014-09-24 08:14:31 -0400

    completed secretz

--- stream-adventure/program.js
+++ stream-adventure/program.js
@@ -1,4 +1,31 @@
+var zlib = require('zlib');
 var crypto = require('crypto');
-var stream = crypto.createDecipher('aes256', process.argv[2]);
+var tar = require('tar');
+var through = require('through');
+var combiner = require('stream-combiner');
+var concat = require('concat-stream');
 
-process.stdin.pipe(stream).pipe(process.stdout);
+
+
+var tarParser = tar.Parse();
+tarParser.on('entry', function (e) {
+  //console.dir(e);
+  tarParser.emit('data', {entry:e});
+});
+
+var app = combiner(
+  crypto.createDecipher(process.argv[2], process.argv[3]),//decrypt
+  zlib.createGunzip(),//decompress
+  tarParser,//extract files
+  through(function(container){
+    var t = this;
+    if (container.entry && container.entry.type === 'File') {
+      container.entry.pipe(crypto.createHash('md5', { encoding: 'hex' })).pipe(concat(function (hash) {
+        t.queue(hash.toString() + " " + container.entry.path+"\n");
+      }));
+    }
+  })
+);
+
+
+process.stdin.pipe(app).pipe(process.stdout);
