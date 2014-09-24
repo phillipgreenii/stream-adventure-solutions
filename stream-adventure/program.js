afd883129a88b714db24534d78e5a53533ca44ae
Phillip Green II <phillip.green.ii@gmail.com>
2014-09-23 23:44:57 -0400

    completed combiner

--- stream-adventure/program.js
+++ stream-adventure/program.js
@@ -1,16 +1,42 @@
-var duplexer = require('duplexer');
+var combine = require('stream-combiner');
+var split = require('split');
 var through = require('through');
+var zlib = require('zlib');
 
-module.exports = function (counter) {
-  var counts = {};
-
-  var tr = through(
-    function write (row) {
-      counts[row.country] = (counts[row.country] || 0) + 1;
-    },
-    function end () {
-      counter.setCounts(counts);
-    });
-
-  return duplexer(tr, counter);
+module.exports = function () {
+  var current_genre,
+      books = [];
+  function dumpBooks(stream) {
+    if (current_genre) {
+      var result = {
+        name: current_genre,
+        books: books
+      };
+      stream.queue(JSON.stringify(result)+"\n");
+    }
+  }
+  return combine(
+    split(),
+    through(
+      function write(line){
+        if (line.length === 0) {
+          return;
+        }
+        var json = JSON.parse(line);
+        if (json.type == 'genre') {
+          dumpBooks(this);
+          current_genre = json.name;
+          books = [];
+        } else if (json.type == 'book') {
+          books.push(json.name);
+        } else {
+          // FIXME should throw error
+        }
+      },
+      function end(){
+        dumpBooks(this);
+        this.queue(null);
+      }),
+    zlib.createGzip()
+  );
 };
