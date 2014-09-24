cbd56074c937487c03fc8700af8bbb1a67ec3af0
Phillip Green II <phillip.green.ii@gmail.com>
2014-09-23 23:57:30 -0400

    complete crypt

--- stream-adventure/program.js
+++ stream-adventure/program.js
@@ -1,42 +1,4 @@
-var combine = require('stream-combiner');
-var split = require('split');
-var through = require('through');
-var zlib = require('zlib');
+var crypto = require('crypto');
+var stream = crypto.createDecipher('aes256', process.argv[2]);
 
-module.exports = function () {
-  var current_genre,
-      books = [];
-  function dumpBooks(stream) {
-    if (current_genre) {
-      var result = {
-        name: current_genre,
-        books: books
-      };
-      stream.queue(JSON.stringify(result)+"\n");
-    }
-  }
-  return combine(
-    split(),
-    through(
-      function write(line){
-        if (line.length === 0) {
-          return;
-        }
-        var json = JSON.parse(line);
-        if (json.type == 'genre') {
-          dumpBooks(this);
-          current_genre = json.name;
-          books = [];
-        } else if (json.type == 'book') {
-          books.push(json.name);
-        } else {
-          // FIXME should throw error
-        }
-      },
-      function end(){
-        dumpBooks(this);
-        this.queue(null);
-      }),
-    zlib.createGzip()
-  );
-};
+process.stdin.pipe(stream).pipe(process.stdout);
